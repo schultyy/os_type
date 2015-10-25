d8c2b10e4ecf013ad34c4f5c5887e38844f94012
Jan Schulte <hello@unexpected-co.de>
2015-10-25 23:41:09 +0100

    New API for current_platform
    This API now returns a struct which contains also the OS version

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::convert::AsRef;
 use std::path::Path;
 mod lsb_release;
 
-///A list of supported operating system types
+///Supported operating system types
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum OSType {
@@ -13,6 +13,14 @@ pub enum OSType {
     OSX,
     Ubuntu,
     Debian
+}
+
+///Contains information about the operating system such as type and version
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct OSInformation {
+    pub operating_system: OSType,
+    pub version: String
 }
 
 fn file_exists<P: AsRef<Path>>(path: P) -> bool {
@@ -31,20 +39,26 @@ fn is_os_x() -> bool {
     }
 }
 
-fn lsb_release() -> OSType {
+fn lsb_release() -> Option<OSInformation> {
     match lsb_release::retrieve() {
         Some(release) => {
             if release.distro == Some("Ubuntu".to_string()) {
-                OSType::Ubuntu
+                Some(OSInformation {
+                    operating_system: OSType::Ubuntu,
+                    version: release.version.unwrap_or("0.0.0".into())
+                })
             }
             else if release.distro == Some("Debian".to_string()) {
-                OSType::Debian
+                Some(OSInformation {
+                    operating_system: OSType::Debian,
+                    version: release.version.unwrap_or("0.0.0".into())
+                })
             }
             else {
-                OSType::Unknown
+                None
             }
         },
-        None => OSType::Unknown
+        None => None
     }
 
 }
@@ -57,17 +71,29 @@ fn lsb_release() -> OSType {
 ///use os_type;
 ///let os = os_type::current_platform();
 ///```
-pub fn current_platform() -> OSType {
+pub fn current_platform() -> OSInformation {
+
+    let unknown_os = OSInformation {
+        operating_system: OSType::Unknown,
+        version: "0.0.0".into()
+    };
+
     if is_os_x() {
-        OSType::OSX
+        OSInformation {
+            operating_system: OSType::OSX,
+            version: "0.0.0".into()
+        }
     }
     else if lsb_release::is_available() {
-        lsb_release()
+        lsb_release().unwrap_or(unknown_os)
     }
     else if file_exists("/etc/redhat-release") || file_exists("/etc/centos-release") {
-        OSType::Redhat
+        OSInformation {
+            operating_system: OSType::Redhat,
+            version: "0.0.0".into()
+        }
     }
     else {
-        OSType::Unknown
+        unknown_os
     }
 }
