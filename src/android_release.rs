5f5bab26e493c3b3d92f71fe7fc46d652a174f48
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2017-06-23 22:48:52 +0100

    Added android support

--- src/android_release.rs
+++ src/android_release.rs
@@ -7,10 +7,12 @@ pub fn get_android_version() -> String {
 
     if output.is_ok() {
         let res = output.unwrap().stdout;
-        let result = format!("{}", String::from_utf8_lossy(&res));
+        let mut result = format!("{}", String::from_utf8_lossy(&res));
 
         let len = result.len();
         result.truncate(len - 1);
+
+        return result;
 
     } else {
         super::default_version()
