4b2212a0f00eaa54be1164e85430f6a64c2748dc
Kyle Bloom <kylejbloom@gmail.com>
2023-09-03 08:26:57 +0100

    Change uname command

--- src/os_information/uname.rs
+++ src/os_information/uname.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 use regex::Regex;
-use utils::*;
+use utils::get_first_capture;
 
 use super::{OSInformation, OSType, TryInformation};
 
@@ -17,7 +17,7 @@ impl TryInformation for Uname {
             let version = r.version.unwrap_or(OSInformation::default_version());
             let distro = r.distro.unwrap_or("".to_string()).to_lowercase();
             match distro.as_str() {
-                "cygwin_nt" => Some(OSInformation::new(OSType::Cygwin, version)),
+                "cygwin" => Some(OSInformation::new(OSType::Cygwin, version)),
                 _ => None,
             }
         })
@@ -26,18 +26,18 @@ impl TryInformation for Uname {
 
 fn retrieve() -> Option<Uname> {
     Command::new("uname")
-        .arg("-a")
+        .arg("-or")
         .output()
         .map(|output| {
             let stdout = String::from_utf8_lossy(&output.stdout);
-            parse(stdout)
+            parse(stdout.trim())
         })
         .ok()
 }
 
 fn parse<S: AsRef<str>>(file: S) -> Uname {
-    let distrib_regex = Regex::new(r#"^(\w+)"#).unwrap();
-    let version_regex = Regex::new(r#"Windows\s(.*?)-"#).unwrap();
+    let distrib_regex = Regex::new(r#"(\w+)$"#).unwrap();
+    let version_regex = Regex::new(r#"^([\w\.]+)"#).unwrap();
 
     let distro = get_first_capture(&distrib_regex, &file);
     let version = get_first_capture(&version_regex, &file);
@@ -51,13 +51,12 @@ mod test {
 
     #[test]
     fn cygwin_3_4_8() {
-        let sample =
-            r#"CYGWIN_NT-10.0-22621 Windows 3.4.8-1.x86_64 2023-08-17 17:02 UTC x86_64 Cygwin"#;
+        let sample = r#"3.4.8-1.x86_64 Cygwin"#;
 
         assert_eq!(
             parse(sample),
             Uname {
-                distro: Some("CYGWIN_NT".to_string()),
+                distro: Some("Cygwin".to_string()),
                 version: Some("3.4.8".to_string()),
             }
         );
