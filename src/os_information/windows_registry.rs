07551954abc2d5707475c9df200aecfe2031f85c
Kyle Bloom <kylejbloom@gmail.com>
2023-09-03 07:23:58 +0100

    Include major and minor number in windows version

--- src/os_information/windows_registry.rs
+++ src/os_information/windows_registry.rs
@@ -14,10 +14,13 @@ impl TryInformation for WindowsRegistry {
             let cur_ver = hklm
                 .open_subkey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
                 .ok()?;
-            cur_ver
-                .get_value("CurrentBuild")
-                .map(|s| OSInformation::new(OSType::Windows, s))
-                .ok()
+            let major_ver: u32 = cur_ver.get_value("CurrentMajorVersionNumber").unwrap_or(0);
+            let minor_ver: u32 = cur_ver.get_value("CurrentMinorVersionNumber").unwrap_or(0);
+            let build_ver: String = cur_ver
+                .get_value("CurrentBuildNumber")
+                .unwrap_or("0".to_string());
+            let version = format!("{major_ver}.{minor_ver}.{build_ver}");
+            Some(OSInformation::new(OSType::Windows, version))
         }
         #[cfg(not(target_os = "windows"))]
         None
